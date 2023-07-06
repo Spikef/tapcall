648b8aa2aa0b0f754c4d8c587ac25b534982cd13
Spikef <spikef@foxmail.com>
2023-07-06 17:51:14 +0800

    feat: SyncLoopHook

--- src/hooks/sync-loop-hook.ts
+++ src/hooks/sync-loop-hook.ts
@@ -8,10 +8,8 @@ export default class SyncLoopHook<
     for (let i = 0; i < this.callbacks.length; i++) {
       const name = this.options[i].name;
       const callback = this.callbacks[i];
-      let result: Return | void | undefined;
-      do {
-        result = this.runCallback(name, callback, args);
-      } while (result !== undefined);
+      const result = this.runCallback(name, callback, args);
+      if (result !== undefined) i = -1;
     }
   }
 }
