8f934247dec8cadcafc37eb4d16bbcc57b52af33
Spikef <spikef@foxmail.com>
2023-07-07 13:13:54 +0800

    feat: build

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import SyncHook from './hooks/sync-hook';
 import SyncBailHook from './hooks/sync-bail-hook';
-import SyncLoopHook from 'tapcall/hooks/sync-loop-hook';
+import SyncLoopHook from './hooks/sync-loop-hook';
 import SyncWaterfallHook from './hooks/sync-waterfall-hook';
 import AsyncParallelHook from './hooks/async-parallel-hook';
 import AsyncParallelBailHook from './hooks/async-parallel-bail-hook';
 import AsyncSeriesHook from './hooks/async-series-hook';
 import AsyncSeriesBailHook from './hooks/async-series-bail-hook';
-import AsyncSeriesLoopHook from 'tapcall/hooks/async-series-loop-hook';
+import AsyncSeriesLoopHook from './hooks/async-series-loop-hook';
 import AsyncSeriesWaterfallHook from './hooks/async-series-waterfall-hook';
 
 export {
